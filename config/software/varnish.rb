192ce6bce20a275dba90c202a9b27e0c816c607f
nathan-opscode <nathan@opscode.com>
2012-09-28 12:03:58 -0700

    more better paths for building varnish

--- config/software/varnish.rb
+++ config/software/varnish.rb
@@ -6,8 +6,24 @@ source :url => "http://repo.varnish-cache.org/source/varnish-3.0.3.tar.gz",
 
 relative_path "varnish"
 
+env =
+  case platform
+  when "solaris2"
+    {
+      "LDFLAGS" => "-R #{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+    }
+  else
+    {
+      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+    }
+  end
+
 build do
-  command "./configure"
-  command "make"
+  command "./configure --prefix=#{install_dir}/embedded", :env => env
+  command "make", :env => env
   command "make install"
 end
