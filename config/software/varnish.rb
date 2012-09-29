56932cde7cda7cba096410a100b4b0bc0a8006ae
Tim Hinderliter <tim@opscode.com>
2012-09-29 12:51:53 -0700

    fixed varnish config steps for ncurses -I directory

--- config/software/varnish.rb
+++ config/software/varnish.rb
@@ -9,23 +9,13 @@ source :url => "http://repo.varnish-cache.org/source/varnish-3.0.3.tar.gz",
 relative_path "varnish-3.0.3"
 
 env =
-  case platform
-  when "solaris2"
-    {
-      "LDFLAGS" => "-R #{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-    }
-  else
-
-    {
-      "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-      "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-      "PCRE_CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
-      "PCRE_LIBS" => "-L#{install_dir}/embedded/lib -lpcre"
-    }
-  end
+  {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -I#{install_dir}/embedded/include/ncurses -static-libgcc",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  "PCRE_CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include -static-libgcc",
+  "PCRE_LIBS" => "-L#{install_dir}/embedded/lib -lpcre"
+}
 
 build do
   command "./configure --prefix=#{install_dir}/embedded", :env => env
