1c6e06f88576a4a91bed3886cbe04f7f6a3de9fa
Kevin Smith <kevin@opscode.com>
2012-11-15 07:43:20 -0500

    Snapshot of WIP

--- cookbooks/chef-server-base/attributes/default.rb
+++ cookbooks/chef-server-base/attributes/default.rb
@@ -1,3 +1,3 @@
 default.base_tar_file     = "chef-server-base.tar.gz"
 default.base_tmp_dir      = "/tmp"
-default.base_tarball_url  = "https://dl.dropbox.com/u/1249734/chef-server-base_1.2.7-112.ubuntu.10.04_amd64.tar.gz"
+default.base_tarball_url  = "https://dl.dropbox.com/u/1249734/chef-server-base-1.2.7-113-ga5758f5-debian.tar.gz"
--- cookbooks/chef-server-base/metadata.rb
+++ cookbooks/chef-server-base/metadata.rb
@@ -2,7 +2,6 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Base Chef Server dependencies"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.10.0"
 
 %w{ ubuntu }.each do |os|
