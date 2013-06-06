32f86cd1c48660c25d518aeb6c61a27e7ffac303
Nathan L Smith <nlloyds@gmail.com>
2013-06-06 16:23:54 -0500

    exclude hosted-only gems from webui2 bundle

--- config/software/opscode-webui2.rb
+++ config/software/opscode-webui2.rb
@@ -13,7 +13,7 @@ source :git => "git@github.com:opscode/opscode-webui2"
 relative_path "opscode-webui2"
 
 build do
-  bundle  "install --without development test --path=/opt/opscode/embedded/service/gem"
+  bundle  "install --without development test hosted --path=/opt/opscode/embedded/service/gem"
   rake    "assets:precompile", :env => { 'PATH' => "/opt/opscode/embedded/bin:$PATH" }
   command "mkdir -p #{install_dir}/embedded/service/opscode-webui2"
   command "#{install_dir}/embedded/bin/rsync -a --delete --delete-excluded --exclude=.git/*** --exclude=.gitignore --exclude=prototype/*** --exclude=tmp ./ #{install_dir}/embedded/service/opscode-webui2/"
