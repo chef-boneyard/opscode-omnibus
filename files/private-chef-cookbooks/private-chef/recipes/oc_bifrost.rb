03a4ab3fd8361baac3c9cd2357c366964ec31f69
Seth Chisamore <schisamo@opscode.com>
2013-07-03 14:14:42 -0400

    INTEGRATION: switch bifrost recipe to runit community cookbook

--- files/private-chef-cookbooks/private-chef/recipes/oc_bifrost.rb
+++ files/private-chef-cookbooks/private-chef/recipes/oc_bifrost.rb
@@ -52,7 +52,7 @@ template "/opt/opscode/embedded/service/oc_bifrost/bin/oc_bifrost" do
   group "root"
   mode "0755"
   variables(node['private_chef']['oc_bifrost'].to_hash)
-  notifies :restart, 'service[oc_bifrost]' if OmnibusHelper.should_notify?("oc_bifrost")
+  notifies :restart, 'runit_service[oc_bifrost]' if OmnibusHelper.should_notify?("oc_bifrost")
 end
 
 oc_bifrost_config = File.join(oc_bifrost_etc_dir, "sys.config")
@@ -61,24 +61,11 @@ template oc_bifrost_config do
   source "oc_bifrost.config.erb"
   mode "644"
   variables(node['private_chef']['oc_bifrost'].to_hash)
-  notifies :restart, 'service[oc_bifrost]' if OmnibusHelper.should_notify?("oc_bifrost")
+  notifies :restart, 'runit_service[oc_bifrost]' if OmnibusHelper.should_notify?("oc_bifrost")
 end
 
 link "/opt/opscode/embedded/service/oc_bifrost/etc/sys.config" do
   to oc_bifrost_config
 end
 
-runit_service "oc_bifrost" do
-  down node['private_chef']['oc_bifrost']['ha']
-  options({
-    :log_directory => oc_bifrost_log_dir,
-    :svlogd_size => node['private_chef']['oc_bifrost']['log_rotation']['file_maxbytes'],
-    :svlogd_num  => node['private_chef']['oc_bifrost']['log_rotation']['num_to_keep']
-  }.merge(params))
-end
-
-if node['private_chef']['bootstrap']['enable']
-  execute "/opt/opscode/bin/private-chef-ctl start oc_bifrost" do
-    retries 20
-  end
-end
+component_runit_service "oc_bifrost"
