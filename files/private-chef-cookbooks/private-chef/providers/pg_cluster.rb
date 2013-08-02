7c674cbe93f92146f79bbf19dc673c0fdfc609b1
Christopher Maier <cm@opscode.com>
2013-08-02 17:39:54 -0400

    pg_cluster LWRP cleanup

--- files/private-chef-cookbooks/private-chef/providers/pg_cluster.rb
+++ files/private-chef-cookbooks/private-chef/providers/pg_cluster.rb
@@ -7,6 +7,9 @@ use_inline_resources
 # Initialize a PostgreSQL database cluster.  Ensures the data
 # directory exists, runs initdb, and sets up postgresql.conf and
 # pg_hba.conf files.
+#
+# Does NOT signal for the cluster to start; that's your responsibility
+# if you want it.
 action :init do
 
   # Ensure the data directoy exists first!
@@ -21,17 +24,15 @@ action :init do
     command "initdb --pgdata #{new_resource.data_dir} --locale C"
     user node['private_chef']['postgresql']['username']
     environment({"PATH" => "/opt/opscode/embedded/bin:#{ENV['PATH']}"})
-    not_if { File.exists?(File.join(new_resource.data.dir, "PG_VERSION")) }
+    not_if { ::File.exists?(::File.join(new_resource.data_dir, "PG_VERSION")) }
   end
 
   # Create configuration files
   ["postgresql.conf", "pg_hba.conf"].each do |config_file|
-    template File.join(new_resource.data_dir, config_file) do
+    template ::File.join(new_resource.data_dir, config_file) do
       owner node['private_chef']['postgresql']['username']
       mode "0644"
       variables(node['private_chef']['postgresql'].to_hash)
-      # TODO: Why "runit_service" instead of "service"?
-      notifies :restart, 'runit_service[postgresql]' if OmnibusHelper.should_notify?("postgresql")
     end
   end
 
