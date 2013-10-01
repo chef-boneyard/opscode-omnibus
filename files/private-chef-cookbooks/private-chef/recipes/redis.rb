90a93a9f6cf7da48380d197565f076e7bd15f134
Marc Paradise <marc@opscode.com>
2013-10-01 10:03:46 -0400

    auto-initialize lb redis configuration

--- files/private-chef-cookbooks/private-chef/recipes/redis.rb
+++ files/private-chef-cookbooks/private-chef/recipes/redis.rb
@@ -1,3 +1,19 @@
+# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# License:: Apache License, Version 2.0
+# Author:: Marc A. Paradise <marc@opscode.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 redis_dir = node['private_chef']['redis']['dir']
 redis_etc_dir = File.join(redis_dir, "etc")
@@ -18,48 +34,53 @@ end
 
 redis_config = File.join(redis_etc_dir, "redis.conf")
 
+redis_data = node['private_chef']['redis']
 template redis_config do
   source "redis.conf.erb"
   owner "root"
   group "root"
   mode "0644"
-  variables(node['private_chef']['redis'].to_hash)
+  variables(redis_data.to_hash)
   notifies :restart, 'service[redis]' if OmnibusHelper.should_notify?("redis")
 end
 
-runit_service "redis" do
-  down node['private_chef']['redis']['ha']
-  options({
-    :log_directory => redis_log_dir,
-    :svlogd_size => node['private_chef']['redis']['log_rotation']['file_maxbytes'],
-    :svlogd_num => node['private_chef']['redis']['log_rotation']['num_to_keep']
-  }.merge(params))
-end
+component_runit_service "redis"
 
-if node['private_chef']['bootstrap']['enable']
-  execute "/opt/opscode/bin/private-chef-ctl start redis" do
-  retries 20
-end
+ruby_block "set_lb_redis_values" do
+  block do
+    require "redis"
+    redis = Redis.new(:host => redis_data.vip, :port => redis_data.port)
+    xdl = node['private_chef']['lb']['xdl_defaults']
+    banned_ips = PrivateChef['banned_ips']
+    maint_mode_ips = PrivateChef['maint_mode_whitelist_ips']
+    redis.hset "dl_default", "503_mode", true
+    # Ensure there is no stale data, but first institute
+    # a brief maint mode to avoid potential misrouting when
+    # we delete old keys.
+    next while not redis.spop("banned_ips").nil?
+    next while not redis.spop("maint_data").nil?
+    keys = redis.hkeys "dl_default"
+    keys.each do |key|
+      redis.del "dl_default", key
+    end
 
-# This will execute with every CCR, however key-value will only
-# be set if they are not already present.
-#ruby_block "set_redis_defaults" do
-#  block do
-#    require "redis"
-#    redis = Redis.new(:host => "localhost", :port => 6379)
-#    # red.hsetnx will only update the specified key if it is not
-#    # already defined.
-#    redis.pipelined do
-#      redis.hsetnx "dl_default", "503_mode",             false
-#      redis.hsetnx "dl_default", "couchdb_checksums",    true
-#      redis.hsetnx "dl_default", "couchdb_cookbooks",    true
-#      redis.hsetnx "dl_default", "couchdb_environments", true
-#      redis.hsetnx "dl_default", "couchdb_roles",        true
-#      redis.hsetnx "dl_default", "couchdb_data",         true
-#      redis.hsetnx "dl_default", "couchdb_clients",      true
-#      redis.hsetnx "dl_default", "disable_new_orgs",     false
-#    end
-#  end
-#  action :create
-#  only_if { enable_service }
-#end
+    # Now we're clear to repopulate from configuration.
+    banned_ips.each do |ip|
+      redis.sadd   "banned_ips", ip
+    end
+    maint_mode_ips.each do |ip|
+      redis.sadd   "maint_data", ip
+    end
+    # Note that we'll preserve 503 mode until everything is
+    # populated.
+    xdl.each do |key, value|
+      redis.hset("dl_default", key, value) unless key == "503_mode"
+    end
+    if xdl.has_key?("503_mode")
+      redis.hset "dl_default", "503_mode", xdl["503_mode"]
+    else
+      redis.hdel "dl_default", "503_mode"
+    end
+  end
+  action :create
+end
