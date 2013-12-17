bf952ab6357213ce61b39d739703c930a54b6152
Ho-Sheng Hsiao <hosh@getchef.com>
2013-12-17 09:31:12 -0800

    Enable sqitch run during reconfigure

--- files/private-chef-cookbooks/private-chef/recipes/erchef_database.rb
+++ files/private-chef-cookbooks/private-chef/recipes/erchef_database.rb
@@ -3,22 +3,14 @@ private_chef_pg_database "opscode_chef" do
   notifies :run, "execute[chef-server-schema]", :immediately
 end
 
-# Though Sqitch runs are idempotent, we need to have them run only
-# when the database is first created (thus the chained notifications).
-# This is only for EC11; subsequent versions can run them
-# idempotently, like normal.  This is just to allow fresh EC11
-# installs to use Sqitch; upgraded installations have a Partybus
-# upgrade to run.  The end result should be that whether or not you
-# are installing or upgrading EC11, at the end of the day, you'll have
-# Sqitch metadata info in your database.
+# Sqitch runs are idempotent. These are turned on
+# assuming that the customer has already upgraded to EC 11.0.0
 execute "chef-server-schema" do
   command "sqitch --db-name opscode_chef deploy --verify"
   # OSC schema is a dependency of the EC schema, and managed by it
   cwd "/opt/opscode/embedded/service/enterprise-chef-server-schema/deps/chef-server-schema"
   user node['private_chef']['postgresql']['username']
   returns [0,1]
-  action :nothing
-  notifies :run, "execute[enterprise-chef-server-schema]", :immediately
 end
 
 execute "enterprise-chef-server-schema" do
@@ -26,7 +18,6 @@ execute "enterprise-chef-server-schema" do
   cwd "/opt/opscode/embedded/service/enterprise-chef-server-schema"
   user node['private_chef']['postgresql']['username']
   returns [0,1]
-  action :nothing
 end
 
 
