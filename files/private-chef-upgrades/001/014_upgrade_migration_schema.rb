d5101ff392a3db4db4e02fd1dc883a84bef8f24b
tylercloke <tyler@opscode.com>
2014-05-28 19:33:12 -0700

    Use make args instead of trying (impossibly) to set env vars from ruby.

--- files/private-chef-upgrades/001/014_upgrade_migration_schema.rb
+++ files/private-chef-upgrades/001/014_upgrade_migration_schema.rb
@@ -5,7 +5,7 @@ define_upgrade do
     must_be_data_master
 
     # run 2.2.4 migration which includes schema upgrade for migration state
-    run_command("EC_TARGET=@2.2.4 OSC_TARGET=@1.0.4 DB_USER=opscode-pgsql make deploy",
+    run_command("make deploy EC_TARGET=@2.2.4 OSC_TARGET=@1.0.4 DB_USER=opscode-pgsql",
                 :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema")
 
   end
