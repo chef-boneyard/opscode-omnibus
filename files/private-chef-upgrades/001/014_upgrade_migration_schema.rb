d2efad5a57faeb7dde01057aeb7ec4400328cde1
tylercloke <tyler@opscode.com>
2014-05-29 12:27:17 -0700

    Set env variables via run_command.

--- files/private-chef-upgrades/001/014_upgrade_migration_schema.rb
+++ files/private-chef-upgrades/001/014_upgrade_migration_schema.rb
@@ -5,8 +5,10 @@ define_upgrade do
     must_be_data_master
 
     # run 2.2.4 migration which includes schema upgrade for migration state
-    run_command("make deploy EC_TARGET=@2.2.4 OSC_TARGET=@1.0.4 DB_USER=opscode-pgsql",
-                :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema")
+    run_command("make deploy",
+                :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema",
+                :env => {"EC_TARGET" => "@2.2.4", "OSC_TARGET" => "@1.0.4", "DB_USER" => "opscode-pgsql"}
+                )
 
   end
 end
