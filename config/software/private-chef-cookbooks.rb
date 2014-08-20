6103aca26e3c0cc7bde4003b063c9d25f1fd544f
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:09:04 -0400

    Update private-chef-cookbooks to use the new Omnibusisms

--- config/software/private-chef-cookbooks.rb
+++ config/software/private-chef-cookbooks.rb
@@ -1,33 +1,58 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# All Rights Reserved.
+#
+
 name "private-chef-cookbooks"
 
 dependency "berkshelf2"
+dependency "uuidtools"
 
-project_name = project.name
-
-source :path => File.expand_path("files/#{project_name}-cookbooks", Config.project_root)
+source path: "#{project.resources_path}/#{name}"
 
 build do
-  gem "install uuidtools --no-rdoc --no-ri -v 2.1.3"
-  command "mkdir -p #{install_dir}/embedded/cookbooks"
-  command "#{install_dir}/embedded/bin/berks install --berksfile=./#{project_name}/Berksfile --path=#{install_dir}/embedded/cookbooks"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  mkdir "#{install_dir}/embedded/cookbooks"
+
+  # Install the cookbooks
+  command "#{install_dir}/embedded/bin/berks install" \
+          " --berksfile=#{project_dir}/private-chef/Berksfile" \
+          " --path=#{install_dir}/embedded/cookbooks", env: env
+
   block do
     File.open("#{install_dir}/embedded/cookbooks/dna.json", "w") do |f|
-      f.puts "{\"run_list\": [ \"recipe[#{project_name}]\" ]}"
+      f.write JSON.fast_generate(
+        run_list: [
+          'recipe[private-chef::default]',
+        ]
+      )
     end
+
     File.open("#{install_dir}/embedded/cookbooks/show-config.json", "w") do |f|
-      f.puts "{\"run_list\": [ \"recipe[#{project_name}::show_config]\" ]}"
+      f.write JSON.fast_generate(
+        run_list: [
+          'recipe[private-chef::show_config]',
+        ]
+      )
     end
+
     File.open("#{install_dir}/embedded/cookbooks/post_upgrade_cleanup.json", "w") do |f|
-      f.puts <<JSON
-{"run_list": ["recipe[#{project_name}::post_11_upgrade_cleanup]",
-              "recipe[#{project_name}::post_12_upgrade_cleanup]"]}
-JSON
+      f.write JSON.fast_generate(
+        run_list: [
+          "recipe[private-chef::post_11_upgrade_cleanup]",
+          "recipe[private-chef::post_12_upgrade_cleanup]",
+        ]
+      )
     end
+
     File.open("#{install_dir}/embedded/cookbooks/solo.rb", "w") do |f|
-      f.puts "CURRENT_PATH = File.expand_path(File.dirname(__FILE__))"
-      f.puts "file_cache_path \"\#\{CURRENT_PATH\}/cache\""
-      f.puts "cookbook_path CURRENT_PATH"
-      f.puts "verbose_logging true"
+      f.write <<-EOH.gsub(/^ {8}/, '')
+        cookbook_path   "#{install_dir}/embedded/cookbooks"
+        file_cache_path "#{install_dir}/embedded/cookbooks/cache"
+        verbose_logging true
+      EOH
     end
   end
 end
