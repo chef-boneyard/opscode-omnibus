03496e30228d5a1eb8be44414145f3d346ad53fa
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:08:30 -0400

    Update opscode-test to use the new Omnibusisms

--- config/software/opscode-test.rb
+++ config/software/opscode-test.rb
@@ -1,29 +1,29 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# All Rights Reserved.
+#
+
 name "opscode-test"
 default_version "0.3.2"
 
 dependency "ruby"
 dependency "libxml2"
-dependency "rsync"
 dependency "bundler"
 dependency "postgresql92"
 
-source :git => "git@github.com:opscode/opscode-test"
-
-# Since this project pulls in the pg gem (or depends on something that
-# does) we need to have the pg_config binary on the PATH so the
-# correct library and header locations can be found
-env = {
-  'PATH' => "#{install_dir}/embedded/bin:#{ENV['PATH']}"
-}
-
-bundle_path = "#{install_dir}/embedded/service/gem"
+source git: "git@github.com:opscode/opscode-test"
 
 build do
-  bundle "install --without dev --path=#{bundle_path}", :env => env
-  command "mkdir -p #{install_dir}/embedded/service/opscode-test"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/opscode-test/"
+  env = with_standard_compiler_flags(with_embedded_path)
 
-  # cleanup the .git directories in the bundle path before commiting
-  # them as submodules to the git cache
-  command "find #{bundle_path} -type d -name .git | xargs rm -rf"
+  bundle "install" \
+         " --path '#{install_dir}/embedded/service/gem'" \
+         " --without dev", env: env
+
+  sync "#{project_dir}", "#{install_dir}/embedded/service/opscode-test/", exclude: ['**/.git', '**/.gitignore']
+
+  # Cleanup the .git directories in the bundle path before commiting them as
+  # submodules to the git cache.
+  command "find #{install_dir}/embedded/service/gem -type d -name .git | xargs rm -rf"
 end
