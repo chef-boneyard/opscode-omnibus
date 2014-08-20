b1025ef61221a0324fed717a82768bf62db5a5e0
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:07:41 -0400

    Update oc_id to use the new Omnibusisms

--- config/software/oc_id.rb
+++ config/software/oc_id.rb
@@ -1,3 +1,9 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# All Rights Reserved.
+#
+
 name "oc_id"
 default_version "0.3.3"
 
@@ -6,20 +12,18 @@ dependency "nodejs"
 dependency "ruby"
 dependency "bundler"
 
-source :git => "git@github.com:opscode/oc-id"
+source git: "git@github.com:opscode/oc-id"
 
 relative_path "oc-id"
 
-env = {
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
-  "LD_FLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
-
 build do
-  bundle "install --path=#{install_dir}/embedded/service/gem --without development test doc", :env => env
-  bundle "exec rake assets:precompile", :env => env
-  command "mkdir -p #{install_dir}/embedded/service/oc_id"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore --exclude=log --exclude=tmp ./ #{install_dir}/embedded/service/oc_id/"
-  command "rm -rf #{install_dir}/embedded/service/oc_id/log"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  bundle "install" \
+         " --path '#{install_dir}/embedded/service/gem'" \
+         " --without development test doc", env: env
+  bundle "exec rake assets:precompile", env: env
+
+  sync "#{project_dir}", "#{install_dir}/embedded/service/oc_id/", exclude: ['**/.git', '**/.gitignore']
+  delete "#{install_dir}/embedded/service/oc_id/log"
 end
