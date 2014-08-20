63c882cc0c1a9deac86a30308f19bef6f0676827
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:08:55 -0400

    Update postgresql92 to use the new Omnibusisms

--- config/software/postgresql92.rb
+++ config/software/postgresql92.rb
@@ -1,18 +1,7 @@
 #
-# Copyright:: Copyright (c) 2012-2013 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2014 Chef Software, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# All Rights Reserved.
 #
 
 name "postgresql92"
@@ -24,37 +13,35 @@ dependency "libedit"
 dependency "ncurses"
 dependency "libossp-uuid"
 
-source :url => "http://ftp.postgresql.org/pub/source/v9.2.9/postgresql-9.2.9.tar.bz2",
-       :md5 => "38b0937c86d537d5044c599273066cfc"
+source url: "http://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2",
+       md5: "38b0937c86d537d5044c599273066cfc"
 
-relative_path "postgresql-9.2.9"
-
-configure_env = {
-  "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib,-rpath,#{install_dir}/embedded/postgresql/9.2/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
+relative_path "postgresql-#{version}"
 
 build do
-  command ["./configure",
-           "--prefix=#{install_dir}/embedded/postgresql/9.2",
-           "--with-libedit-preferred",
-           "--with-openssl",
-           "--with-ossp-uuid",
-           "--with-includes=#{install_dir}/embedded/include",
-           "--with-libraries=#{install_dir}/embedded/lib"].join(" "), :env => configure_env
-  command "make world -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
-  command "make install-world"
+  env = with_standard_compiler_flags(with_embedded_path)
 
-  # Postgres 9.2 is our "real" Postgres installation (prior versions
-  # that are installed are solely to facilitate upgrades).  As a
-  # result, we need to have the binaries for this version available
-  # with the other binaries used by Private Chef.  This one-liner is
-  # probably the easiest way to do that.
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded/postgresql/9.2" \
+          " --with-libedit-preferred" \
+          " --with-openssl" \
+          " --with-ossp-uuid" \
+          " --with-includes=#{install_dir}/embedded/include" \
+          " --with-libraries=#{install_dir}/embedded/lib", env: env
+
+  make "world -j #{max_build_jobs}", env: env
+  make "install-world", env: env
+
+  # Postgres 9.2 is our "real" Postgres installation (prior versions that are
+  # installed are solely to facilitate upgrades). As a result, we need to have
+  # the binaries for this version available with the other binaries used by
+  # Private Chef.
   #
-  # NOTE: The single-quotes wrapping the inner command are required;
-  # otherwise, we end up getting some kind of weird Inception-style
-  # variable interpolation, such that the value of $BIN in each
-  # iteration of the loop is the last file in the directory.
- command "sh -c 'for BIN in #{install_dir}/embedded/postgresql/9.2/bin/*; do ln -s ${BIN} #{install_dir}/embedded/bin/$(basename ${BIN}); done'"
+  # This must happen in a Ruby block or else the computation will happen at
+  # load time instead of build time (and the files will not yet exist on disk).
+  block do
+    Dir.glob("#{install_dir}/embedded/postgresql/9.2/bin/*").each do |path|
+      FileUtils.ln_s(path, "#{install_dir}/embedded/bin/#{File.basename(path)}")
+    end
+  end
 end
