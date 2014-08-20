006a24dcf42051acb9e2fc680070a67be323e726
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:07:50 -0400

    Update openresty-lpeg to use the new Omnibusisms

--- config/software/openresty-lpeg.rb
+++ config/software/openresty-lpeg.rb
@@ -1,20 +1,22 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# All Rights Reserved.
+#
+
 name "openresty-lpeg"
 default_version "0.12"
 
 dependency "openresty"
 
-source :url => "http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-#{version}.tar.gz",
-       :md5 => "4abb3c28cd8b6565c6a65e88f06c9162"
+source url: "http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-#{version}.tar.gz",
+       md5: "4abb3c28cd8b6565c6a65e88f06c9162"
 
 relative_path "lpeg-#{version}"
 
-env = {
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
-}
-
-lua_dir = "#{install_dir}/embedded/luajit/include/luajit-2.0"
-
 build do
-  command "make LUADIR=#{lua_dir}", :env => env
-  command "install -p -m 0755 lpeg.so #{install_dir}/embedded/lualib"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  make "LUADIR='#{install_dir}/embedded/luajit/include/luajit-2.0'", env: env
+  command "install -p -m 0755 lpeg.so #{install_dir}/embedded/lualib", env: env
 end
