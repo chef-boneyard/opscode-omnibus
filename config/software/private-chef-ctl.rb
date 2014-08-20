7666a0e2221a483bedd4c4d9ddfbffa5caa3fc16
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:09:08 -0400

    Update private-chef-ctl to use the new Omnibusisms

--- config/software/private-chef-ctl.rb
+++ config/software/private-chef-ctl.rb
@@ -1,42 +1,24 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# All Rights Reserved.
+#
+
 name "private-chef-ctl"
 
-dependency "rsync"
 dependency "highline-gem"
 dependency "omnibus-ctl"
 
-source :path => File.expand_path("files/private-chef-ctl-commands", Config.project_root)
+source path: "#{project.resources_path}/#{name}"
 
 build do
-  block do
-    open("#{install_dir}/bin/private-chef-ctl", "w") do |file|
-      file.print <<-EOH
-#!/bin/bash
-#
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
-#
-# All Rights Reserved
-#
+  erb source: "private-chef-ctl.erb",
+      dest:   "#{install_dir}/bin/private-chef-ctl",
+      mode:   0755,
+      vars:   {
+        embedded_bin: "#{install_dir}/embedded/bin",
+        embedded_service: " #{install_dir}/embedded/service",
+      }
 
-export SVWAIT=30
-
-# Ensure the calling environment (disapproval look Bundler) does not infect our
-# Ruby environment if private-chef-ctl is called from a Ruby script.
-for ruby_env_var in RUBYOPT \\
-                    BUNDLE_BIN_PATH \\
-                    BUNDLE_GEMFILE \\
-                    GEM_PATH \\
-                    GEM_HOME
-do
-  unset $ruby_env_var
-done
-
-#{install_dir}/embedded/bin/omnibus-ctl opscode #{install_dir}/embedded/service/omnibus-ctl $@
-       EOH
-    end
-  end
-
-  command "chmod 755 #{install_dir}/bin/private-chef-ctl"
-
-  # additional omnibus-ctl commands
-  command "#{install_dir}/embedded/bin/rsync -a ./ #{install_dir}/embedded/service/omnibus-ctl/"
+  sync "#{project_dir}", "#{install_dir}/embedded/service/omnibus-ctl/", exclude: ['**/.git', '**/.gitignore']
 end
