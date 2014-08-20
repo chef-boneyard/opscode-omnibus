8992a73bb2dd49bb5cc97d2960c4394f15a2688a
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:06:59 -0400

    Update knife-ec-backup-gem to use the new Omnibusisms

--- config/software/knife-ec-backup-gem.rb
+++ config/software/knife-ec-backup-gem.rb
@@ -1,18 +1,7 @@
 #
-# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2014 Chef Software, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# All Rights Reserved.
 #
 
 name "knife-ec-backup"
@@ -21,10 +10,16 @@ default_version "2.0.0.beta.2"
 dependency "pg-gem"
 dependency "sequel-gem"
 
-# Ignore dependencies, since they are already installed on the system by omnibus
-# (see dependencies above)
-# This ensures we can properly link the pg to the needed headers without needing
-# to pass options through knife-ec-backup
 build do
-  gem "install knife-ec-backup -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version} --ignore-dependencies"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # Ignore dependencies, since they are already installed on the system by
+  # omnibus from one of the dependencies. This ensures we can properly link the
+  # pg to the needed headers without needing to pass options through
+  # knife-ec-backup.
+  gem "install knife-ec-backup" \
+      " --version '#{version}'" \
+      " --bindir '#{install_dir}/embedded/bin'" \
+      " --ignore-dependencies" \
+      " --no-ri --no-rdoc", env: env
 end
