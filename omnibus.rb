e92809d1109a94389e4d16a1e7b36c4129de88e6
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:49:51 -0400

    Hardcode the cache bucket

--- omnibus.rb
+++ omnibus.rb
@@ -7,7 +7,7 @@
 use_s3_caching true
 s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-s3_bucket      ENV['AWS_S3_BUCKET']
+s3_bucket      'opscode-omnibus-cache'
 
 # Customize compiler bits
 # ------------------------------
